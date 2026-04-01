95d30f6128d255a2179c4ff947a50046cc1fd632
Alumno <alumno@et26.local>
2026-04-01 15:20:04 -0300

    abby2

---
+++ ejercicio2.js
@@ -0,0 +1,15 @@
+function ejercicio2() {
+    var valores = [true, 5, false, "hola", "adios", 2];
+    var texto1 = valores[3]; // "hola"
+    var texto2 = valores[4]; // "adios"
+}
+
+if(texto1 > texto2){
+     console.log("El texto mayor es:", texto1, ".")
+console.log("El texto menor es:", texto2, ".")
+    }
+    {}
+var verdadero = valores[0]
+ var falso = valores [2]
+ console.log("resultado verdadero:", resultadotrue);
+    console.log("resultado falso:", resultadofalso);
---
+++ index.js
@@ -0,0 +1,37 @@
+function ejercicio1() {
+    const meses = [
+        "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+        console.log(meses);
+}
+function ejercicio2() {
+    var valores = [true, 5, false, "hola", "adios", 2];
+    var texto1 = valores[3]; // "hola"
+    var texto2 = valores[4]; // "adios"
+}
+function ejercicio2() {
+    
+if(texto1 > texto2){
+     console.log("El texto mayor es:", texto1, ".")
+console.log("El texto menor es:", texto2, ".")
+    }
+    {}
+var verdadero = valores[0]
+ var falso = valores [2]
+ console.log("resultado verdadero:", resultadotrue);
+    console.log("resultado falso:", resultadofalso);
+{
+    var numero1 = 5;
+var numero2 = 8;
+if(numero1<numero2) {
+console.log("numero1 no es mayor que numero2");
+}
+if(numero2 > 0) {
+console.log("numero2 es positivo");
+}
+if(numero1 < 0 || numero1 != 0) {
+console.log("numero1 es negativo o distinto de cero");
+}
+if() {
+console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+}
