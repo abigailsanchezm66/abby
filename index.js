1ca90006e3c5b535db5cecdf0c60489f73103dab
lucascasas456-debug <lucascasas456@gmail.com>
2026-04-08 15:31:47 -0300

    abby8-4

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ var verdadero = valores[0]
  console.log("resultado verdadero:", resultadotrue);
     console.log("resultado falso:", resultadofalso);
 {
+}
     var numero1 = 5;
 var numero2 = 8;
 if(numero1<numero2) {
@@ -34,16 +35,15 @@ console.log("numero2 es positivo");
 if(numero1 < 0 || numero1 != 0) {
 console.log("numero1 es negativo o distinto de cero");
 }
-if() {
+if(++numero1 < numero2) {
 console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
 }
 }
 
-function ejercico4 () {
 //*EJERCICIO 4:El cálculo de la letra del Documento Nacional de Identidad (DNI) es un proceso matemáticosencillo que se basa en obtener el resto de la división entera del número de DNI y el número23. A partir del resto de la división, se obtiene la letra seleccionándola dentro de un array deletras//*
  //*protn = variable Por tanto si el resto de la división es 0, la letra del DNI es la T y si el resto es 3 la letra es laA. Con estos datos, elaborar un pequeño script que:Almacene en una variable el número de DNI indicado por el usuario y en otra variable laletra del DNI que se ha indicado.En primer lugar (y en una sola instrucción) se debe comprobar si el número es menor que mayor que 99999999. Si ese es el caso, se muestra un mensaje al usuario indicando queel número proporcionado no es válido y el programa no muestra más mensajes.Si el número es válido, se calcula la letra que le corresponde según el método explicadoanteriormente.Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si nocoinciden, se muestra un mensaje al usuario diciéndole que la letra que ha indicado no escorrecta. En otro caso, se muestra un mensaje indicando que el número y la letra de DNIson correctos.//*
 
- function ejercicio5() {
+ function ejercicio4() {
 
 var numeroDNI = parseInt(prompt("Introduce tu número de DNI:"));
 var letraUsuario = prompt("Introduce la letra de tu DNI:");
@@ -63,6 +63,7 @@ else {
         alert("El número y la letra de DNI son correctos.");
     }
 }
+ }
 
 function ejercico5 () {
 //*EJERCICIO 5:El factorial de un número entero n es una operación matemática que consiste en multiplicatodos los factores n x (n-1) x (n-2) x ... x 1. Así, el factorial de 5 (escrito como 5!) es igual a:5! = 5 x 4 x 3 x 2 x 1 = 120Utilizando la estructura for, crear un script que calcule el factorial de un número entero.//*
@@ -81,10 +82,10 @@ document.writeln("<h2>El factorial de " + numero + "! es: " + resultado + "</h2>
 
 function ejercicio6() {
 
-if (valor%2==0) //Si el if da verdadero luego es par.
+if (valor%2==0){} //Si el if da verdadero luego es par.
 
-let par = 0
-let inpar = 0
+let par = 0;
+let inpar = 0;
 
 for(let i=1; i<11; i++){
 
@@ -105,12 +106,38 @@ console.log("cantidad de valores Inpares", inpar)
 console.log("cantidad de valores Pares", par)
 
 //*EJERCICIO 7=Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas. === comparacion estricta
- 
+ function ejercicio7() {
 pront = texto 
 texto = promp("Definir y comparar texto")
-if (texto===texto.toUpperCase)
+    var texto= promt("ingrese algo menos un numero");
+    if (!isNan(texto) || texto == null || texto ==" "){
+        console.log("denegado")
     }
-else if (texto=== texto.tolower(ad))
+}else{
+if (textp===texto.toUpperCase){
+    document.writeIn("esta escrito en mayuscula");
     }
-jswddjswkdwk kekwdnwkdnw dJDLSKFdddeererqwwqweqweqweq
+}else if{(texto=== texto.tolower(ad)){}
+    document.writeln("esta escrito con minuscula");
+}
+ }else{
+    document.writeIn("es una mezclaaaa");
+ }
+ //EJERCICIO 10=Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la funciónMath.random para obtener números aleatorios entre 1 y 6 para cada uno de loslanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array elnúmero de apariciones de dicha suma, repitiendo 36.000 veces esta operación.
+function ejercicio10() {
+ let arr = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+ for (let i=0; i <36000; i++){
 
+    let dado1= Math.floor(Math.random()*6)+1;
+    let dado2= Math.floor(Math.random()*6)+1;
+    let suma= dado1 + dado2;
+
+    arr[suma] = arr[suma] +1;
+ }
+
+ console.log(arr);
+
+ for (let j = 2; j < arr.length; j++) {
+    console.log("El numero: "+j+" salio "+arr[j]+" veces")
+ }
+}
